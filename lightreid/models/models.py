ee9f78e9e256f85a7b42474f0d1ab785ccf3aff8
Shuo Yang <yangshuo129@gmail.com>
2020-10-27 16:59:41 +0800

    fix bugs

--- lightreid/models/models.py
+++ lightreid/models/models.py
@@ -14,7 +14,7 @@ class BaseReIDModel(nn.Module):
     combine backbone, pooling and head modules
     """
 
-    def __init__(self, backbone, pooling, head, norm_type='softmax'):
+    def __init__(self, backbone, pooling, head, norm_type="softmax"):
         super(BaseReIDModel, self).__init__()
         self.backbone = backbone
         self.pooling = pooling
@@ -22,12 +22,14 @@ class BaseReIDModel(nn.Module):
         self.use_tanh = False
         self.norm_type = norm_type
 
-    def forward(self, x, y=None, teacher_mode=False, fixcnn=False, test_feat_from_head=True):
-        '''
+    def forward(
+        self, x, y=None, teacher_mode=False, fixcnn=False, test_feat_from_head=True
+    ):
+        """
         Args:
             x(torch.tensor): input images
             y(torch.tensor): input labels, used for such as circle loss
-        '''
+        """
         # cnn backbone
         feats_map = self.backbone(x)
         if fixcnn:
@@ -41,21 +43,22 @@ class BaseReIDModel(nn.Module):
         # teacher mode
         if teacher_mode:
             headfeats_vec, logits = self.head(
-                feats_vec, y, use_tanh=self.use_tanh, teacher_mode=True)
+                feats_vec, y, use_tanh=self.use_tanh, teacher_mode=True
+            )
             return feats_vec, headfeats_vec, logits
 
         # return
         if self.training:
-            if self.head.__class__.__name__ == 'BNHead':
-                headfeats_vec, logits = self.head(
-                    feats_vec, y, use_tanh=self.use_tanh)
+            if self.head.__class__.__name__ == "BNHead":
+                headfeats_vec, logits = self.head(feats_vec, y, use_tanh=self.use_tanh)
                 return feats_vec, headfeats_vec, logits
-            elif self.head.__class__.__name__ == 'CodePyramid':
+            elif self.head.__class__.__name__ == "CodePyramid":
                 feats_list, headfeats_list, logits_list = self.head(
-                    feats_vec, y, use_tanh=self.use_tanh)
+                    feats_vec, y, use_tanh=self.use_tanh
+                )
                 return feats_list, headfeats_list, logits_list
             else:
-                assert 0, 'head error'
+                assert 0, "head error"
         else:
             if test_feat_from_head:
                 headfeats_vec = self.head(feats_vec, y, use_tanh=self.use_tanh)
@@ -90,31 +93,34 @@ def self_similarity(featuremaps):
     b, d, h, w = featuremaps.shape
     # print("featuremaps shape:", featuremaps.shape, "==>", (b, d, h, w))
     kernels = featuremaps.permute([0, 2, 3, 1])  # [b, h, w, d]
-    similarities = torch.zeros(b, 2 * h*w, device=featuremaps.device)
+    similarities = torch.zeros(b, 2 * h * w, device=featuremaps.device)
     for i in range(b):
         kernel = kernels[i]  # [h, w, d]
         featuremap = featuremaps[i].unsqueeze_(0)  # [1, d, h, w]
         # print("featuremap shape:", featuremap.shape)
         kernel = kernel.reshape(-1, d, 1, 1)  # [hw, d, 1, 1]
-        similarity = F.conv2d(featuremap, kernel)  # [1,hw, h, w]
+        similarity = nn.functional.conv2d(featuremap, kernel)  # [1,hw, h, w]
         # print("similarity shape: ", similarity.shape)
-        similarity = similarity.view(1, h*w, h*w)
+        similarity = similarity.view(1, h * w, h * w)
         # print("similarity shape: ", similarity.shape)
         similarity = torch.cat(
-            (similarity.max(dim=1)[0], similarity.max(dim=2)[0]), dim=-1)
+            (similarity.max(dim=1)[0], similarity.max(dim=2)[0]), dim=-1
+        )
         # print("similarity shape: ", similarity.shape)
         similarities[i] = similarity
     return similarities
 
 
-def self_similarity2(featuremaps, norm_type='softmax'):
+def self_similarity2(featuremaps, norm_type="softmax"):
     # norm_type: l1 or softmax
     assert norm_type in [
-        'l1', 'softmax'], "norm_type of BaseReIDModel should be 'l1' or 'softmax'"
+        "l1",
+        "softmax",
+    ], "norm_type of BaseReIDModel should be 'l1' or 'softmax'"
     b, d, h, w = featuremaps.shape
     # print("featuremaps shape:", featuremaps.shape, "==>", (b, d, h, w))
     kernels = featuremaps.permute([0, 2, 3, 1])  # [b, h, w, d]
-    similarities = torch.zeros(b, h*w, device=featuremaps.device)
+    similarities = torch.zeros(b, h * w, device=featuremaps.device)
     # similarities = torch.zeros(b, 2 * h*w, device=featuremaps.device)
     for i in range(b):
         kernel = kernels[i]  # [h, w, d]
@@ -123,15 +129,16 @@ def self_similarity2(featuremaps, norm_type='softmax'):
         kernel = kernel.reshape(d, -1)  # [hw, d, 1, 1]
         similarity = torch.matmul(featuremap, kernel)  # [hw, hw]
         # print("similarity shape: ", similarity.shape)
-        if norm_type == 'l1':
-            similarity = similarity/torch.norm(similarity, p=1, dim=-1).expand(
-                similarity.size()[1], similarity.size()[0]).transpose(0, 1)
-        elif norm_type == 'softmax':
-            similarity = F.softmax(similarity, dim=-1)
+        if norm_type == "l1":
+            similarity = similarity / torch.norm(similarity, p=1, dim=-1).expand(
+                similarity.size()[1], similarity.size()[0]
+            ).transpose(0, 1)
+        elif norm_type == "softmax":
+            similarity = nn.functional.softmax(similarity, dim=-1)
         # similarity = similarity/torch.norm(similarity)
         # similarity = F.softmax(similarity)
         # print(similarity.sum(-1))
-        similarity = similarity.view(1, h*w, h*w)
+        similarity = similarity.view(1, h * w, h * w)
         # print("similarity shape: ", similarity.shape)
         # similarity = torch.cat(
         #     (similarity.max(dim=1)[0], similarity.max(dim=2)[0]), dim=-1)
@@ -141,9 +148,10 @@ def self_similarity2(featuremaps, norm_type='softmax'):
     return similarities
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import numpy as np
-    tem = np.random.random(3*5*2*5)
+
+    tem = np.random.random(3 * 5 * 2 * 5)
     featuremaps = torch.tensor(tem).reshape((3, 5, 2, 5)).cuda()
     similarities = self_similarity2(featuremaps.float())
     # similarities = self_similarity2(featuremaps.float(), 'softmax')
